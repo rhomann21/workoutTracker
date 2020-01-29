4f717651dbd838d1af245cc3732a90f1ed672be8
Rachael Homann <rachaelhomann@gmail.com>
2020-01-28 21:09:25 -0600

    class update jan 28

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -1,3 +1,13 @@
-//route for exercise.html
+const router = require('express').Router();
+const path = require('path');
 
-//route for stats.html
+//route for exercise.html
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/exercise.html"));
+  });
+
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/stats.html"));
+  });
+
+module.exports = router;
--- README.md
+++ README.md
@@ -7,6 +7,5 @@
 - Mongoose
 
 ## Directions:
-Run an MPN init to install a package-json.
-Use ```node server.js``` to run the app in the console.
+Run an MPN init to install a package-json. Use ```node server.js``` to run the app in the console. 
 
